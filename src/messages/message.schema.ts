5d74f2ed33a11aa517d2af91e8a44d8318cd4d4f
Jack <jack@bykollab.com>
2023-11-01 18:24:56 +0300

    add msg scheme

---
+++ src/messages/message.schema.ts
@@ -0,0 +1,58 @@
+import * as mongoose from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+// message:{
+
+//     ChatId > Dialog
+// Type: user/service
+// Text
+// Media > []Media
+// atCreated
+// atEdited
+// OriginalText
+// Id
+// CounterNumber
+// }
+export const messageSchema = new mongoose.Schema({
+  chatId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Dialog',
+  },
+  type: {
+    type: String,
+    enum: ['user', 'service'],
+  },
+  text: String,
+  media: [String],
+  atCreated: Date,
+  atEdited: Date,
+  originalText: String,
+  Id: String,
+  counterNumber: Number,
+});
+export type MessageDocument = HydratedDocument<Message>;
+
+@Schema()
+export class Message {
+  @Prop()
+  chatId: string;
+  @Prop()
+  type: string;
+  @Prop()
+  text: string;
+  @Prop()
+  media: string[];
+  @Prop()
+  atCreated: Date;
+  @Prop()
+  atEdited: Date;
+  @Prop()
+  originalText: string;
+  @Prop()
+  Id: string;
+  @Prop()
+  counterNumber: number;
+}
+
+export const MessageSchema = SchemaFactory.createForClass(Message);
